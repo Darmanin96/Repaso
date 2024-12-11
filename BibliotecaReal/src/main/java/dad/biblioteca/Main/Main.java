b76770729b05700999a8e271965fb1d17d181230
Darmanin96 <165905936+Darmanin96@users.noreply.github.com>
2024-12-11 15:44:04 +0000

    Delete BibliotecaReal directory

--- BibliotecaReal/src/main/java/dad/biblioteca/Main/Main.java
+++
@@ -1,9 +0,0 @@
-package dad.biblioteca.Main;
-
-import javafx.application.*;
-
-public class Main {
-    public static void main(String[] args){
-        Application.launch(BibliotecaApp.class,args);
-    }
-}
